468f8eec5f02a1cb5fcc9a471d7b346b1c63e9ff
mrochereau2024 <mehdi.rochereau@hotmail.fr>
2024-11-27 16:55:54 +0100

    ok

--- src/main/java/demo/jardigenie/ihm/ArticleController.java
+++ src/main/java/demo/jardigenie/ihm/ArticleController.java
@@ -3,11 +3,17 @@ package demo.jardigenie.ihm;
 
 import demo.jardigenie.bll.IArticleManager;
 import demo.jardigenie.bo.Article;
+import demo.jardigenie.bo.Category;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -18,7 +24,7 @@ public class ArticleController {
     private IArticleManager articleManager;
 
     @GetMapping("/list-articles")
-    public String showListArticles (Model model) {
+    public String showListArticles(Model model) {
 
         List<Article> articles = articleManager.getAllArticles();
 
@@ -28,7 +34,7 @@ public class ArticleController {
     }
 
     @GetMapping("/details-article/{id}")
-    public String showDetailArticle (@PathVariable Long id, Model model) {
+    public String showDetailArticle(@PathVariable Long id, Model model) {
 
         Article article = articleManager.getArticleById(id);
 
@@ -41,8 +47,51 @@ public class ArticleController {
         return "details/details-article";
     }
 
+    @GetMapping({"/show-article-form/{id}", "/show-article-form"})
+    public String showFormArticle(@PathVariable(required = false) Long id, Model model) {
+
+        Article article = new Article();
+
+        if (id != null) {
+            article = articleManager.getArticleById(id);
+        }
+
+        model.addAttribute("article", article);
+
+        List<Category> categories = articleManager.getAllCategories();
+
+        model.addAttribute("categories", categories);
+
+        return "form/form-article";
+    }
+
+    @PostMapping("/article-form")
+    public String articleForm(@Valid @ModelAttribute("article") Article article, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        List<Category> categories = articleManager.getAllCategories();
+
+        model.addAttribute("categories", categories);
+
+        if (bindingResult.hasErrors()) {
+            return "form/form-article";
+        }
+
+        if (article.getId() == null) {
+            articleManager.saveArticle(article);
+            return "redirect:/list-articles";
+        }
+
+        if (article.getId() != null) {
+            articleManager.updateArticle(article);
+            return "redirect:/list-articles";
+        }
+
+        return "redirect:/";
+    }
+
+
     @GetMapping("/")
-    public String home () {
-        return "base";
+    public String home() {
+        return "home";
     }
 }
